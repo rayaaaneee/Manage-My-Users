713f64e06952da596e84db4614e7210ff80baea4
rayanelecrackdecettegeneration <92400806+rayanemrln@users.noreply.github.com>
2023-12-14 00:34:17 +0100

    init

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  /* { path: '', pathMatch: 'full', redirectTo: 'users' },
+  { path: '**', redirectTo: 'users' | '404},
+  { path: 'users', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailsComponent },
+  { path: 'update/:id', component: UpdateUserComponent },
+  { path: 'add', component: AddUserComponent }, */
+];
